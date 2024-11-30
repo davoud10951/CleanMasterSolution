35b105625c0f7571e5e3b6f56cbdfffe0b6d2b58
d.qarajeh <d.qarajeh@D-QARAJEH>
2024-11-30 17:25:54 +0330

    Add project files.

---
+++ SampleCleanProject/03.Endpoints/SampleCleanProject.Endpoints.Api/Controllers/GharajehBaseController.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleCleanProject.Endpoints.Api.Controllers
+{
+    [Route("[controller]/[action]"), ApiController]
+    public class GharajehBaseController : ControllerBase
+    {
+        private ISender? _mediator;
+        protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+    }
+}
